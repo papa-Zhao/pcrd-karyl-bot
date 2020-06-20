a2313d74d4394ead51a9ed47ac00f80abb79a469
zhao boru <r22742557@gmail.com>
2020-06-20 15:48:18 +0800

    delete port

--- app.py
+++ app.py
@@ -175,4 +175,4 @@ def handle_message(event):
 if __name__ == "__main__":
     # app.debug = True
     # port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0')
